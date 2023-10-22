0da5d44b2703982eb7b9153d4f85bc50132a1a98
CloudLun <cloud830902@gmail.com>
2023-10-22 14:31:20 -0400

    change the recommended movies' margin bottom

--- components/element/main/Recommend.tsx
+++ components/element/main/Recommend.tsx
@@ -20,12 +20,12 @@ const Recommend = () => {
     return (
         <div className='flex-auto px-[16px] md:px-0 lg:w-[calc(100vw-200px)] h-[calc(100vh-338px)] md:h-[calc(100vh-527px)] lg:h-[calc(100vh-471px)] overflow-y-scroll'>
             <h2 className='mb-[24px] lg:mb-[32px] font-regular text-heading_small text-white'>Recommended for you</h2>
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[15px] md:gap-x-[30px] lg:gap-x-[40px] gap-y-[60px] md:gap-y-[80px] pb-[60px] '>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[15px] md:gap-x-[30px] lg:gap-x-[40px] gap-y-[60px] md:gap-y-[80px] lg:gap-y-[90px] pb-[60px] '>
                 {
                     data.filter((d, i) => i > 4).map((d, i) => {
                         return (
                             <div key={i} className='relative rounded-[8px]'>
-                                <div className='w-[164px] md:w-[220px] h-[110px] md:h-[140px] lg:h-[174px]'>
+                                <div className='w-[164px] md:w-[220px] h-[110px] md:h-[140px] lg:h-[184px]'>
                                     <Image
                                         src={d.thumbnail.regular.large}
                                         fill
@@ -33,7 +33,7 @@ const Recommend = () => {
                                         className='rounded-[8px]'
                                     />
                                 </div>
-                                <div className='absolute top-[118px] md:top-[148px] lg:top-[182px]'>
+                                <div className='absolute top-[118px] md:top-[148px] lg:top-[192px]'>
                                     <div className='mb-[2px] flex items-center gap-[8px] text-[11px] lg:text-body_medium text-white opacity-60'>
                                         <div>{d.year}</div>
                                         <div className='rounded-full h-[3px] w-[3px] bg-white'></div>
@@ -49,7 +49,7 @@ const Recommend = () => {
                                         width={11.67}
                                         height={14}
                                         alt="logo"
-                                        className=''
+                                        className='cursor-pointer'
                                         onClick={() => bookmarkClickHandler(i)}
                                     />
                                 </div>
